e2f2b3fd78e4f0d881fc9ef38d1479ab38f7ce42
whyte624 <whyte624@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 14:50:33 +0000

    suppress warnings, no-favicon attribute
    git-svn-id: https://plugins.svn.wordpress.org/favicon-links/trunk@1089114 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- favicon-links.php
+++ favicon-links.php
@@ -1,39 +1,51 @@
 <?php
+
 /**
-* Plugin Name: Favicon Links
-* Plugin URI: http://whyte624.ru/prj/wordpress-favicon-links
-* Description: Adds favicons to all links in any post
-* Version: 1.0
-* Author: Andrei Sozonov
-* Author URI: http://whyte624.ru/
-* License: GPL2
-*/
+ * Plugin Name: Favicon Links
+ * Plugin URI: http://whyte624.ru/prj/wordpress-favicon-links
+ * Description: Adds favicons to all links in any post
+ * Version: 1.1
+ * Author: Andrei Sozonov
+ * Author URI: http://whyte624.ru/
+ * License: GPL2
+ */
 
-function favicon_links_the_content($content) {
-    $html = new DOMDocument(null, 'UTF-8');
-    $html->loadHTML('<meta http-equiv="content-type" content="text/html; charset=utf-8">' . $content);
+function favicon_links_the_content($content)
+{
+    try {
+        $html = new DOMDocument(null, 'UTF-8');
+        @$html->loadHTML('<meta http-equiv="content-type" content="text/html; charset=utf-8">' . $content);
 
-    foreach ($html->getElementsByTagName('a') as $a) {
-        /* @var DOMElement $a */
-        $imgs = $a->getElementsByTagName('img');
-        if ($imgs->length > 0) {
-            continue;
+        foreach ($html->getElementsByTagName('a') as $a) {
+            /* @var DOMElement $a */
+            $imgs = $a->getElementsByTagName('img');
+            if ($imgs->length > 0) {
+                continue;
+            }
+
+            $url = $a->attributes->getNamedItem('href');
+            if (!$url) {
+                continue;
+            }
+            $urlParts = parse_url($url->textContent);
+            if (!$urlParts || empty($urlParts['host'])) {
+                continue;
+            }
+
+            if ($a->attributes->getNamedItem('data-no-favicon')) {
+                continue;
+            }
+
+            $img = $html->createElement('img');
+            $img->setAttribute('src', 'http://www.google.com/s2/favicons?domain=' . $urlParts['host']);
+            $img->setAttribute('style', 'padding-right: 4px;');
+            $a->insertBefore($img, $a->firstChild);
         }
 
-        $url = $a->attributes->getNamedItem('href');
-        if (!$url) {
-            continue;
-        }
-        $urlParts = parse_url($url->textContent);
-        if (!$urlParts || empty($urlParts['host'])) {
-            continue;
-        }
-
-        $img = $html->createElement('img');
-        $img->setAttribute('src', 'http://www.google.com/s2/favicons?domain=' . $urlParts['host']);
-        $a->insertBefore($img, $a->firstChild);
+        return $html->saveHTML();
+    } catch (Exception $e) {
+        return $content;
     }
-
-    return $html->saveHTML();
 }
-add_filter( 'the_content', 'favicon_links_the_content' );
+
+add_filter('the_content', 'favicon_links_the_content');
--- readme.txt
+++ readme.txt
@@ -1,18 +1,41 @@
 === Favicon Links ===
 Contributors: whyte624
-Tags: favicon, link, a, href, url, google, image
+Tags: favicon, link, a, href, url, google, image, decoration
+Stable tag: trunk
 Donate link: http://whyte624.ru
 Requires at least: 4.0
 Tested up to: 4.1
 License: GPL2
 
-Adds favicons to links in posts.
+Adds favicons to links in posts to give them nice look.
+
+== Installation ==
+Install from plugin directory or download files from https://github.com/whyte624/wordpress-favicon-links and put them into 'plugins' directory of your website.
 
 == Description ==
 The plugin adds favicons of websites to links which they lead to.
 If a link has image inside (img tag), then favicon will not be added.
 
+To suppress favicon for your link add 'data-no-favicon' attribute to the a tag:
+<a href="http://example.com" target="_blank" data-no-favicon>my link</a>.
+
 Uses Google S2 Favicon service.
 
+Report issues or/and contribute here: https://github.com/whyte624/wordpress-favicon-links.
+
+== Changelog ==
+= 1.1 =
+* Suppress warnings for wrong html markup
+* Small padding after favicon for nicer look
+* Suppress favicon for specific link
+
+= 1.1 =
+* Basic functionality
+
 == Screenshots ==
-1. https://raw.githubusercontent.com/whyte624/wordpress-favicon-links/master/screenshots/screenshot-1.png
+1. Example of Favicon Links plugin output
+2. Example of Favicon Links plugin output
+
+== Frequently Asked Questions ==
+
+== Upgrade Notice ==
